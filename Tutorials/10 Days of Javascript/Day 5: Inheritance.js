7ad41bb706445fe44cbf338f474f4121fd40a861
Alexander Bauer <mail@alexanderbauer.org>
2020-06-05 22:35:23 +0300

    Create Day 5: Inheritance.js

---
+++ Tutorials/10 Days of Javascript/Day 5: Inheritance.js
@@ -0,0 +1,28 @@
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+}
+
+/*
+ *  Write code that adds an 'area' method to the Rectangle class' prototype
+ */
+
+/*
+ * Create a Square class that inherits from Rectangle and implement its class constructor
+ */
+
+Rectangle.prototype.area = function(){
+    return this.w * this.h;
+}
+
+class Square extends Rectangle {
+
+    constructor(s){
+        super(s);
+            this.h = s;
+            this.w = s;
+    }
+
+}
