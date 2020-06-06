658482b3fe55155abf6d8f20ad668b19b1b4cb1d
Alexander Bauer <mail@alexanderbauer.org>
2020-06-06 13:27:36 +0300

    Create Day 7: Arrays.py

---
+++ Tutorials/30 Days of Code/Day 7: Arrays.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n   =   int(input())
+
+    arr =   list(map(int, input().rstrip().split()))
+
+    res =   ''
+    for x in reversed(arr):
+        res += str(x) + ' '
+
+    print(res)
