4baa0ac01dbcc6bdcf9b141d345615d932e3b4b5
Alexander Bauer <mail@alexanderbauer.org>
2020-06-06 12:30:25 +0300

    Update Day 1: Data Types.py
    added variables i, d, s

--- Tutorials/30 Days of Code/Day 1: Data Types.py
+++ Tutorials/30 Days of Code/Day 1: Data Types.py
@@ -1,3 +1,6 @@
+i               =   4
+d               =   4.0
+s               =   'HackerRank '
 # Declare second integer, double, and String variables.
 second_integer  =   False
 double          =   False
