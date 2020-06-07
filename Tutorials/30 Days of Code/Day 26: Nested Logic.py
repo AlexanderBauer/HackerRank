656818eb9c6ce05226b705224106cefcb41df908
Alexander Bauer <mail@alexanderbauer.org>
2020-06-07 14:39:10 +0300

    Create Day 26: Nested Logic.py

---
+++ Tutorials/30 Days of Code/Day 26: Nested Logic.py
@@ -0,0 +1,23 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+def fine():
+    # split input into list, structure: [D, M, Y]
+    actual      =   input().split()
+    expected    =   input().split()
+
+    # calculate differences
+    day_diff    =   int(actual[0]) - int(expected[0])
+    month_diff  =   int(actual[1]) - int(expected[1])
+    year_diff   =   int(actual[2]) - int(expected[2])
+
+    # calculate fines
+    if(day_diff <= 0 and month_diff <= 0 and year_diff <= 0 or year_diff < 0):
+        return 0
+    elif(day_diff >= 0 and month_diff == 0 and year_diff == 0):
+        return 15 * day_diff
+    elif(month_diff >= 0 and year_diff == 0):
+        return 500 * month_diff
+    elif year_diff > 0:
+        return 10000
+
+print(fine())
