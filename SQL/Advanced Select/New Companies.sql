7070f551f8d423f2721f8b19894fe30b6f2e48dc
Alexander Bauer <mail@alexanderbauer.org>
2020-06-12 23:48:44 +0300

    Update New Companies.sql
    replaced USING with ON

--- SQL/Advanced Select/New Companies.sql
+++ SQL/Advanced Select/New Companies.sql
@@ -1,5 +1,11 @@
-SELECT DISTINCT company_code, founder, COUNT(DISTINCT(lead_manager_code)), COUNT(DISTINCT(senior_manager_code)), COUNT(DISTINCT(manager_code)), COUNT(DISTINCT(employee_code)) 
-FROM company INNER JOIN Employee 
-USING(company_code) 
+SELECT DISTINCT company.company_code, 
+        founder, 
+        COUNT(DISTINCT(lead_manager_code)), 
+        COUNT(DISTINCT(senior_manager_code)), 
+        COUNT(DISTINCT(manager_code)), 
+        COUNT(DISTINCT(employee_code)) 
+FROM company 
+INNER JOIN Employee 
+ON company.company_code = Employee.company_code
 GROUP BY company_code, founder 
 ORDER BY company_code ASC;
