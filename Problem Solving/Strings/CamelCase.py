c8f92301f66c1f839527b5e1ef0a25155b890ac4
Alexander Bauer <mail@alexanderbauer.org>
2020-06-15 22:57:30 +0300

    Create CamelCase.py

---
+++ Problem Solving/Strings/CamelCase.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the camelcase function below.
+def camelcase(s):
+    r = re.findall('([A-Z])', s)
+    return len(r) + 1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
