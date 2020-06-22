51c87e7981b7bba7e2e90eb97149c6b8ae312ca9
Alexander Bauer <mail@alexanderbauer.org>
2020-06-22 17:59:28 +0300

    Create Detect HTML Tags, Attributes and Attribute Values.py

---
+++ Python/Regex and Parsing/Detect HTML Tags, Attributes and Attribute Values.py
@@ -0,0 +1,23 @@
+from html.parser import HTMLParser
+import sys
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print (tag)
+        if(attrs):
+            for x in attrs:
+                print('-> ' + str(x[0]) + ' > ' + str(x[1]))
+    def handle_endtag(self, tag):
+        pass
+    def handle_startendtag(self, tag, attrs):
+        print (tag)
+        if(attrs):
+            for x in attrs:
+                print('-> ' + str(x[0]) + ' > ' + str(x[1]))
+    def handle_comment(self, data):
+        pass
+
+# instantiate the parser and fed it some HTML
+parser = MyHTMLParser()
+parser.feed(sys.stdin.read())
