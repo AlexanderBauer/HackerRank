34f2a7855da9ee39897e3069b17afa77267fd6f8
Alexander Bauer <mail@alexanderbauer.org>
2020-06-23 21:18:46 +0300

    Create Counting Valleys.py

---
+++ Interview Preparation Kit/Warm-up Challenges/Counting Valleys.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingValleys function below.
+def countingValleys(n, s): 
+    ls      =    list(s) 
+    valleys =   0 
+    ups     =   0 
+    downs   =   0 
+    for step in ls: 
+        if step == 'U': 
+            ups += 1 
+            if(ups == downs): 
+                valleys += 1 
+        else: 
+            downs += 1 
+    return valleys
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    s = input()
+
+    result = countingValleys(n, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
